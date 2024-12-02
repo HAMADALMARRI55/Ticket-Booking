4c24a131c9a848e6c533a2592ed8f003a46eaf2c
HAMADALMARRI55 <hamadmarmeri@gmail.com>
2024-12-03 02:08:37 +0530

    Update constants.py

---
+++ AdventureLandTicketingSystem/constants.py
@@ -0,0 +1,11 @@
+# Maximum number of tickets allowed per user
+MAX_TICKETS_PER_USER = 10
+
+# Discount rates
+DISCOUNT_ON_TWO_DAY_PASS = 0.10
+DISCOUNT_ON_GROUP_TICKET = 0.20
+
+# File paths
+FILE_PATH_USERS = "data/users.pkl"
+FILE_PATH_TICKETS = "data/tickets.pkl"
+FILE_PATH_SALES_REPORTS = "data/sales_reports.pkl"
